ef0c23e74e48da31dd76c89ed8c8201df16b15fc
Rob Moore <rob.moore@memberminderpro.com>
2023-08-14 12:25:30 -0500

    changed scripts to service workers and started modifying background.js

--- src/background.js
+++ src/background.js
@@ -1,11 +1,14 @@
 'use strict';
 
+//NOTE - Manifest v2
 chrome.browserAction.onClicked.addListener((tab) => {
   const url = tab.url;
   const title = tab.title;
   const DELAY = 3000;
 
-  chrome.tabs.create({ url: "https://app.standardnotes.org" }, (tab) => {
+  
+//NOTE - Manifest v2
+chrome.tabs.create({ url: "https://app.standardnotes.org" }, (tab) => {
     chrome.tabs.executeScript(tab.id, { file: 'src/contentScript.js', runAt: 'document_end' });
     setTimeout(() => { chrome.tabs.sendMessage(tab.id, { id: 'CREATE_NOTE', url, title }); }, DELAY);
   });
